b1b520084ab20c21299fdc20060ae3eaf721213f
luhuan <751372658@qq.com>
2017-04-06 21:03:24 +0800

    添加picasso封装

---
+++ rxprovider/src/main/java/com/luhuan/rxprovider/RxCountDown.java
@@ -0,0 +1,27 @@
+package com.luhuan.rxprovider;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+
+/**
+ * Created by Administrator on 2017/4/6 0006.
+ */
+
+public class RxCountDown {
+    public static Observable<Integer> countDown(int time) {
+        final int countTime = time;
+        return Observable.interval(0, 1, TimeUnit.SECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<Long, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Long aLong) throws Exception {
+                        return countTime - aLong.intValue();
+                    }
+                }).take(countTime + 1);
+    }
+}
