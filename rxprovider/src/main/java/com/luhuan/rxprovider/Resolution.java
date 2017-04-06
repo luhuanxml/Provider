e03665d1e59d4eb1cbc56cdec5fef8c05fc7f593
luhuan <751372658@qq.com>
2017-04-06 19:46:31 +0800

    添加dp和px的转换工具

---
+++ rxprovider/src/main/java/com/luhuan/rxprovider/Resolution.java
@@ -0,0 +1,21 @@
+package com.luhuan.rxprovider;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2017/4/6 0006.
+ */
+
+public class Resolution {
+
+    public static int dpToPx(Context context, float dp_Size) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp_Size * scale + 0.5f);
+    }
+
+    public static int pxToDp(Context context, float px_Size) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (px_Size / scale + 0.5f);
+    }
+
+}
