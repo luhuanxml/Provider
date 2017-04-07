f06c3b821f8ee1cfa20ab5433d3a18a20f1f23e3
luhuan <751372658@qq.com>
2017-04-07 10:31:08 +0800

    添加倒计时工具类

--- app/src/main/java/com/luhuan/provider/MainActivity.java
+++ app/src/main/java/com/luhuan/provider/MainActivity.java
@@ -2,12 +2,43 @@ package com.luhuan.provider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.luhuan.rxprovider.RxCountDown;
+import com.luhuan.rxprovider.RxToast;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends Activity {
-
+    TextView time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RxToast.init(this);
+        time= (TextView) findViewById(R.id.time);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        RxCountDown.countDown(10).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(@NonNull Integer integer) throws Exception {
+                time.setText(integer + "");
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(@NonNull Throwable throwable) throws Exception {
+
+            }
+        }, new Action() {
+            @Override
+            public void run() throws Exception {
+                RxToast.show("计时结束");
+            }
+        });
     }
 }
