b78ba4509e7f99220a5eb30c94bfd8e2fcc02502
lh <751372658@qq.com>
2017-04-16 10:04:30 +0800

    添加retrofit log日志

--- rxprovider/src/main/java/com/luhuan/rxprovider/RetrofitProvider.java
+++ rxprovider/src/main/java/com/luhuan/rxprovider/RetrofitProvider.java
@@ -1,10 +1,12 @@
 package com.luhuan.rxprovider;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -15,6 +17,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
  */
 
 public class RetrofitProvider {
+    private static final String TAG = "RetrofitProvider";
     private static String ENDPOINT;
     private RetrofitProvider(){
         
@@ -29,10 +32,18 @@ public class RetrofitProvider {
         private static final Retrofit INSTANCE =create();
 
         private static Retrofit create() {
+            HttpLoggingInterceptor loggingInterceptor=new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+                @Override
+                public void log(String message) {
+                    Log.d(TAG, "Retrofit: "+message);
+                }
+            });
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
             OkHttpClient.Builder builder=new OkHttpClient.Builder();
             builder.writeTimeout(10, TimeUnit.SECONDS);
             builder.connectTimeout(10,TimeUnit.SECONDS);
             builder.readTimeout(10,TimeUnit.SECONDS);
+            builder.addInterceptor(loggingInterceptor);
             return new Retrofit.Builder().baseUrl(ENDPOINT)
                     .client(builder.build())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
